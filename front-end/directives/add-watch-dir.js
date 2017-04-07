b0a177c04209a563cf3f051370b013ff6bb962f8
Seth Sutherland <cokolatepal@gmail.com>
2017-04-07 17:50:13 -0600

    still workin...

--- front-end/directives/add-watch-dir.js
+++ front-end/directives/add-watch-dir.js
@@ -1,3 +1,13 @@
 /**
  * Created by Seth on 4/7/2017.
  */
+angular.module('myApp').directive('addWatchDir', function(){
+
+    return {
+        templateUrl: '../templates/add-watch-dir.html',
+        restrict : 'E',
+        link: function(scope, element, attr){
+            
+        }
+    }
+});

c7b50d5dcb6bdc969e5fc530124924f0a8b3eafe
Seth Sutherland <cokolatepal@gmail.com>
2017-04-10 10:57:56 -0600

    Working on some directives.

--- front-end/controllers/manage-controller.js
+++ front-end/controllers/manage-controller.js
@@ -3,13 +3,4 @@
  */
 angular.module('myApp').controller('manageCtrl', function($scope, mainService){
 
-    $scope.addWatch = function(obj){
-        console.log('yo');
-        mainService.addWatch(obj).then(function(response){
-            console.log(response);
-            if(response.status === 200){
-                alert('Watch added.')
-            }
-        })
-    };
 });
--- front-end/directives/add-watch-dir.js
+++ front-end/directives/add-watch-dir.js
@@ -6,8 +6,19 @@ angular.module('myApp').directive('addWatchDir', function(){
     return {
         templateUrl: '../templates/add-watch-dir.html',
         restrict : 'E',
+        controller: function($scope, mainService){
+            $scope.addWatch = function(obj){
+                console.log('yo');
+                mainService.addWatch(obj).then(function(response){
+                    console.log(response);
+                    if(response.status === 200){
+                        alert('Watch added.')
+                    }
+                })
+            };
+        },
         link: function(scope, element, attr){
-            
+
         }
     }
 });
